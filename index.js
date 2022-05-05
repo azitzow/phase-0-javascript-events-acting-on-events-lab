860e296e0ee1bc5b763dd8582d566745e9cd1563
Ana Zitzow <anasempai89@gmail.com>
2022-05-04 18:28:39 -0700

    commit tree b2c946edb1a5e90e6a51656e08eea9c3104d328c

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+
+document.addEventListener('keydown', (event) => {
+  if(event.key === 'ArrowLeft') {
+    moveDodgerLeft();
+  }
+  if(event.key === 'ArrowRight') {
+    moveDodgerRight();
+  }
+})
+
+const moveDodgerLeft = () => {
+  const leftNumbers = dodger.style.left.replace('px', '');
+  const left = parseInt(leftNumbers, 10);
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+const moveDodgerRight = () => {
+  const leftNumbers = dodger.style.left.replace('px', '');
+  const left = parseInt(leftNumbers, 10);
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
